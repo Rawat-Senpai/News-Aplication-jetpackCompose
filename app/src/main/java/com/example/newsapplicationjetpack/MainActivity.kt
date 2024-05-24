7ee16d73c73b7e9c6fd4c17568056669383da2d0
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-05-24 15:22:06 +0530

    Ui part is done after this api calling remains

--- app/src/main/java/com/example/newsapplicationjetpack/MainActivity.kt
+++ app/src/main/java/com/example/newsapplicationjetpack/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,6 +22,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -45,6 +47,7 @@ import com.example.newsapplicationjetpack.screens.SportsScreen
 import com.example.newsapplicationjetpack.ui.theme.NewsApplicationJetpackTheme
 import com.example.newsapplicationjetpack.utils.Screens
 import com.example.newsapplicationjetpack.viewModels.MainViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -71,6 +74,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NewsApplicationJetpackTheme {
+
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
+
+
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)){
                     val startDestination = viewModel.startDestination
                     NavGraph(startDestination = startDestination)
